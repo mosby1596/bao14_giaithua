526ac578988c6e75467c3e2b92716d94c11f08fe
mosby1596 <hoang159609@gmail.com>
2024-11-18 11:24:59 +0700

    Add project files.

---
+++ bao14_giaithua.cpp
@@ -0,0 +1,20 @@
+// bao14_giaithua.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "tinh giai thua\n";
+	int so, giaithua=1;
+	cout << "nhap so:\n";
+	cin >> so;
+		for (int i = 1; i <=so ; i++)
+		{
+			giaithua *= i;
+		}
+		cout << so << "!" << "=" << giaithua << endl;
+		return 0;
+}
+
+
